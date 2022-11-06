456ed88873335dbd39540e032700759bff676296
Otavio Lemes <otavio11lemes@outlook.com>
2022-11-06 19:50:40 -0300

    Interligando as classes de domínio

--- src/br/com/dio/desafio/Main.java
+++ src/br/com/dio/desafio/Main.java
@@ -1,11 +1,10 @@
 package br.com.dio.desafio;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
-import br.com.dio.desafio.dominio.Conteudo;
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
@@ -26,9 +25,43 @@ public class Main {
 		mentoria.setDescricao("descrição mentoria java");
 		mentoria.setDate(LocalDate.now());
 		
+		Bootcamp bootcamp = new Bootcamp();
+		bootcamp.setName("Bootcamp Java Developer");
+		bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+		bootcamp.getConteudos().add(curso1);
+		bootcamp.getConteudos().add(curso2);
+		bootcamp.getConteudos().add(mentoria);
+		
+		Dev devCamila = new Dev();
+		devCamila.setName("Camila");
+		devCamila.inscreverBootcamp(bootcamp);
+		System.out.println("Conteúdos Inscritos Camila: " + devCamila.getConteudosInscritos());
+		devCamila.progredir();
+		System.out.println("-");
+		System.out.println("Conteúdos Inscritos Camila: " + devCamila.getConteudosInscritos());
+		System.out.println("Conteúdos Concluídos Camila: " + devCamila.getConteudosConcluidos());
+		
+		System.out.println("------------------");
+		
+		Dev devJoao = new Dev();
+		devCamila.setName("Joao");
+		devJoao.inscreverBootcamp(bootcamp);
+		System.out.println("Conteúdos Inscritos Joao: " + devJoao.getConteudosInscritos());
+		devJoao.progredir();
+		System.out.println("-");
+		System.out.println("Conteúdos Inscritos Joao: " + devJoao.getConteudosInscritos());
+		System.out.println("Conteúdos Concluídos Joao: " + devJoao.getConteudosConcluidos());
+		
+		
+		
+		
+		
+		
+		/*
 		System.out.println(curso1);
 		System.out.println(curso2);
 		System.out.println(mentoria);
+		*/
 
 	}
 
